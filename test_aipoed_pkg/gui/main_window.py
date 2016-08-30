bafcd6ef0393606ee10e3a17e643b43dacc63a33
Peter Williams <pwil3058@gmail.com>
2016-08-30 15:25:59 +1000

    dialogue: add an AskerMixin to simplify the asking of simple questions
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- test_aipoed_pkg/gui/main_window.py
+++ test_aipoed_pkg/gui/main_window.py
@@ -28,7 +28,10 @@ recollect.define("main_window", "last_geometry", recollect.Defn(str, ""))
 class MainWindow(dialogue.Window):
     def __init__(self):
         dialogue.Window.__init__(self, type=Gtk.WindowType.TOPLEVEL)
-        self.add(BITester())
+        vbox = Gtk.VBox()
+        vbox.pack_start(BITester(), expand=False, fill=True, padding=0)
+        vbox.pack_start(AskerTester(), expand=False, fill=True, padding=0)
+        self.add(vbox)
         self.connect("destroy", lambda _widget: Gtk.main_quit())
         self.parse_geometry(recollect.get("main_window", "last_geometry"))
         self.show_all()
@@ -49,3 +52,19 @@ class BITester(Gtk.HBox, dialogue.BusyIndicatorUser):
                 self._label.set_text(str(i))
                 dialogue.yield_to_pending_events()
         button.set_sensitive(True)
+
+class AskerTester(Gtk.HBox, dialogue.AskerMixin):
+    def __init__(self):
+        Gtk.HBox.__init__(self)
+        self._label = Gtk.Label("Response")
+        ask_button = Gtk.Button.new_with_label(_("Ask Question"))
+        self.pack_start(self._label, expand=True, fill=True, padding=0)
+        self.pack_start(ask_button, expand=False, fill=True, padding=0)
+        ask_button.connect("clicked", self._ask_button_clicked_cb)
+        self.show_all()
+    def _ask_button_clicked_cb(self, button):
+        answer = self.ask_question("A question that needs answering with OK or Cancel")#, "Elaboration would go here.")
+        if answer == Gtk.ResponseType.OK:
+            self._label.set_text("OK")
+        else:
+            self._label.set_text("Cancel")
