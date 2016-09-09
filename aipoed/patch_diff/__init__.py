42093fd03e64d56000213cf0420e553d6de7265c
Peter Williams <pwil3058@gmail.com>
2016-09-09 13:57:07 +1000

    patch_diff: create a new sub package for basic patch/diff code
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- aipoed/patch_diff/__init__.py
+++ aipoed/patch_diff/__init__.py
@@ -1,6 +1,6 @@
 ### -*- coding: utf-8 -*-
-###  Copyright 2016 Peter Williams <pwil3058@gmail.com>
 ###
+###  Copyright (C) 2016 Peter Williams <pwil3058@gmail.com>
 ###
 ### This program is free software; you can redistribute it and/or modify
 ### it under the terms of the GNU General Public License as published by
@@ -14,21 +14,3 @@
 ### You should have received a copy of the GNU General Public License
 ### along with this program; if not, write to the Free Software
 ### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-
-import os
-import gettext
-
-APP_NAME = "test_aipoed"
-BUG_TRACK_URL = "https://github.com/pwil3058/aipoed/issues"
-DISCUSSION_GRP = "pwil3058@gmail.com"
-CONFIG_DIR_PATH = os.path.expanduser(os.path.join("~", ".config", APP_NAME))
-
-if not os.path.exists(CONFIG_DIR_PATH):
-    os.makedirs(CONFIG_DIR_PATH)
-
-import aipoed
-
-# Lets tell those details to gettext
-gettext.install(APP_NAME, localedir=aipoed.i18n.find_locale_dir())
-
-aipoed.options.initialize(CONFIG_DIR_PATH)
